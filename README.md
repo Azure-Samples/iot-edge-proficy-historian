2a2ad9e84b94d191802f00129abc8a8031a7b2fa
David L. Bojsen <david.bojsen@microsoft.com>
2017-03-15 23:54:11 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
-# Contributing
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments
 
-This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments.
+# Proficy Historian Module for the Azure IoT Gateway SDK
+This reference implementation demonstrates how the Azure IoT Gateway SDK can be used to connect to existing Proficy Historian servers and send JSON encoded telemetry data from these servers using the Proficy Historian SDK pub/sub format (using a JSON payload) to Azure IoT Hub. All transport protocols supported by the Gateway SDK can be used, i.e. HTTPS, AMQP and MQTT. The transport is selected in the transport setting in gateway_config.json.
+
+This module uses the Proficy Historian SDK and therefore licensing restrictions from GE may apply. 
+
+# Azure IoT Gateway SDK compatibility
+The current version of the Proxy module is targeted at the [Azure IoT Gateway SDK 2017-01-13 release](https://github.com/Azure/azure-iot-gateway-sdk/releases/tag/2017-01-13).
+
+Use the following command line to clone the compatible version Azure IoT Gateway SDK, then follow the build instructions included:
+
+```
+git clone --recursive https://github.com/Azure/azure-iot-gateway-sdk.git
+```
+# Directory Structure
+
+## /samples
+This folder contains a sample configuration that instructs a vanilla gateway host load the module and IoT Hub proxy module and configures the module to create a 
+subscription on a standard server which publishes the specified tags to Azure IoT Hub.
+
+## /src
+This folder contains the C# Proficy Historian module source file (GatewayModule.cs).
+
+# Building the Module
+
+Open the solution in Visual Studio and run the build command 
+
+# Configuring the Module
+Proficy Historian tags whose values should be published to Azure IoT Hub can be configured in the module JSON configuration.  A sample template configuration file can be found in ```samples/gateway_config.json```.  The configuration consists of a OPC-UA Application Configuration and Subscriptions section.  
+
+## Application Configuration section
+The ```args``` section should be confiugred as shown below  
+
+``` JSON
+"args": {
+    "ServerName": "<HistorianServerName>", //Mandatory
+    "UserName": "<HistorianUserName>", //Mandatory
+    "Password": "<HistorianPassWord>", //Mandatory
+    "PrintToConsole": false, // Optional - default is false
+    "TagsToSubscribe": [ ... ]
+    ]
+}
+```
+
+## TagsToSubscribe section
+The ```TagsToSubscribe``` section contains an array of Tagnames that should be subscribed to from the Proficy Historian server at startup.
+
+``` JSON
+    "TagsToSubscribe": [
+        { "TagName": "<Tagname1>" }, // Tagname is mandatory, MinimumElapsedMilliSeconds is optional - default is 1000
+        { "TagName": "<Tagname2>", "MinimumElapsedMilliSeconds": 1000 }
+    ]
+
+```
+
+# Running the module
+
+To run the module and have it publish to IoT Hub, configure the name of your Hub (JSON field ```"IoTHubName"```) and the IoT Hub device ID and shared access key to use (JSON fields ```"Id"``` and ```"SharedAccessKey"```) in your version of ```gateway_config.json```.  Ensure that the right native module is configured, based on your platform (i.e. iothub.dll for Windows, libiothub.so for Linux, etc.).
---
+++ src/GatewayModule.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.IoT.Gateway;
+using Proficy.Historian.ClientAccess.API;
+using Newtonsoft.Json;
+
+// Module for collecting data from GE (Proficy) Historian database
+namespace Proficy.Historian.Module
+{
+    /// <summary>
+    /// Gateway module - Connects to GE (Proficy) Historian on specified intervals and get specified values for further processing
+    /// </summary>
+    public class GatewayModule : IGatewayModule, IGatewayModuleStart
+    {
+        // Variables regarding gateway
+        private Broker _broker;
+        private Dictionary<string, string> _messageProperties = new Dictionary<string, string>();
+
+        // Variables regarding Proficy Historian
+        private ServerConnection _historian;
+        private ProficyHistorianConfiguration _config;
+
+        /// <summary>
+        /// This function initiates the module and create all the connections
+        /// </summary>
+        /// <param name="broker">The broker of the Gateway this Module should run on</param>
+        /// <param name="configuration">The json configuration for the module.</param>
+        public void Create(Broker broker, byte[] configuration)
+        {
+            Console.WriteLine("Proficy.Historian.Module initializing...");
+
+            // Initialize module
+            this._broker = broker;
+
+            // Get configuration
+            try
+            {
+                string configurationString = System.Text.Encoding.UTF8.GetString(configuration);
+                _config = JsonConvert.DeserializeObject<ProficyHistorianConfiguration>(configurationString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Proficy.Historian.Module not initialized - error: " + ex.Message);
+                Destroy();
+            }
+
+            // Header for messages
+            _messageProperties.Add("source", "Proficy.Historian.Module");
+            _messageProperties.Add("name", "data");
+
+            Console.WriteLine("Proficy.Historian.Module initialized.");
+        }
+
+        /// <summary>
+        /// Start the data collection
+        /// </summary>
+        public void Start()
+        {
+            Console.WriteLine("Proficy.Historian.Module starting up...");
+
+            try
+            {
+                // Define connection and establish it
+                _historian = new ServerConnection(new ConnectionProperties { ServerHostName = _config.ServerName, Username = _config.UserName, Password = _config.Password, ServerCertificateValidationMode = CertificateValidationMode.None });
+                _historian.Connect();
+
+                // establish event handler
+                _historian.DataChangedEvent += new DataChangedHandler(Historian_DataChangedEvent);
+
+                // Setup data subscriptions
+                foreach (ProficyHistorianTag tag in _config.TagsToSubscribe)
+                {
+                    _historian.IData.Subscribe(new DataSubscriptionInfo { Tagname = tag.TagName, MinimumElapsedMilliSeconds = tag.MinimumElapsedMilliSeconds });
+                    Console.WriteLine("Proficy.Historian.Module - subscribing to " + tag.TagName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Proficy.Historian.Module - Error while initializing: " + ex.Message);
+                Destroy();
+            }
+
+            Console.WriteLine("Proficy.Historian.Module started...");
+        }
+
+        /// <summary>
+        /// Handle data change events from the Historian
+        /// </summary>
+        /// <param name="values">The list of changed elements</param>
+        private void Historian_DataChangedEvent(List<CurrentValue> values)
+        {
+            // Run thru all received data changes
+            foreach (CurrentValue cv in values)
+            {
+                // Publish to broker
+                PublishTag(cv.Tagname, cv.Value.ToString(), cv.Time, cv.Quality.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Wraps data nicely in json and publishes to the gateway broker
+        /// </summary>
+        /// <param name="TagName">Name of the tag</param>
+        /// <param name="TagValue">Value of the tag</param>
+        /// <param name="TagDateTime">Datetime of the observed value change</param>
+        /// <param name="TagQuality">The reported quality of the value</param>
+        public void PublishTag(string TagName, string TagValue, DateTime TagDateTime, string TagQuality)
+        {
+            // Wrap data nicely before releasing them to the broker
+            messageWrap mw = new messageWrap();
+            mw.content[0] = new sensorData(TagName, TagValue, TagDateTime.ToString("yyyy-MM-dd HH:mm:ss"), TagQuality);
+            string json = "[" + JsonConvert.SerializeObject(mw, Formatting.None) + "]";
+            Microsoft.Azure.IoT.Gateway.Message messageToPublish = new Microsoft.Azure.IoT.Gateway.Message(json, _messageProperties);
+
+            // Publish to broker
+            this._broker.Publish(messageToPublish);
+
+            // If applicable - print to console window the message sent
+            if (_config.PrintToConsole) Console.WriteLine("Proficy.Historian.Module - sent: " + json);
+        }
+
+        /// <summary>
+        /// This function will dispose this Module
+        /// </summary>
+        public void Destroy()
+        {
+            Console.WriteLine("Proficy.Historian.Module closing.");
+
+            try
+            {
+                // Drop subscriptions and remove event handler
+                _historian.IData.DropSubscriptions();
+                _historian.DataChangedEvent -= Historian_DataChangedEvent;
+
+                // Close connection
+                _historian.Disconnect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Proficy.Historian.Module - error while disposing: " + ex.Message);
+            }
+
+            Console.WriteLine("Proficy.Historian.Module closed.");
+        }
+
+        /// <summary>
+        /// This function is for processing messages from the broker (Not implemented as this module is strictly a data source)
+        /// </summary>
+        /// <param name="received_message">The message to be processed</param>
+        public void Receive(Microsoft.Azure.IoT.Gateway.Message received_message)
+        {
+            // Function must be implemented, but this module shouldn't process data - it is only a data source.
+            // In future scenario, should be able to receive new list of tags to subscribe, and possibly also requests for batch exports.
+        }
+    }
+
+    /// <summary>
+    /// Class for handling efficient message wrapping on broker, reducing payload size
+    /// </summary>
+    public class messageWrap
+    {
+        public string name = "Historian";
+        public sensorData[] content = new sensorData[1];
+    }
+
+    /// <summary>
+    /// Call for handling individual tag readings
+    /// </summary>
+    public class sensorData
+    {
+        public string t;
+        public string v;
+        public string dt;
+        public string q;
+
+        // Initialize the class while populating the variables
+        public sensorData(string TagName, string TagValue, string TagDateTime, string TagQuality)
+        {
+            this.t = TagName;
+            this.v = TagValue;
+            this.dt = TagDateTime;
+            this.q = TagQuality;
+        }
+    }
+
+    /// <summary>
+    /// Wrapper for handling the configuration of the module
+    /// </summary>
+    public class ProficyHistorianConfiguration
+    {
+        public string ServerName;
+        public string UserName;
+        public string Password;
+        public bool PrintToConsole = false;
+        public IList<ProficyHistorianTag> TagsToSubscribe;
+    }
+
+    /// <summary>
+    /// Wrapper for handling the tags to subscribe to
+    /// </summary>
+    public class ProficyHistorianTag
+    {
+        public string TagName;
+        public int MinimumElapsedMilliSeconds = 1000;
+    }
+}
